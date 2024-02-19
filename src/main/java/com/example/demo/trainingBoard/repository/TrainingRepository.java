bf92ab7744ab7b199aefdf57d1077495c5213acd
man-coding <chch2857@gmail.com>
2024-02-19 13:38:12 +0900

    240219 패키지 카테고리 정리 최종(모두 비어 있음)

--- src/main/java/com/example/demo/trainingBoard/repository/TrainingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.trainingBoard.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.trainingBoard.entity.Training;
-
-public interface TrainingRepository extends JpaRepository<Training, Integer> {
-
-}
