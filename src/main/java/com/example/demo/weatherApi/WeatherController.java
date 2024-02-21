64192e8b84d6db1ca118faf8fdcde8e6e923987c
man-coding <chch2857@gmail.com>
2024-02-21 13:52:31 +0900

    날씨 api

--- src/main/java/com/example/demo/weatherApi/WeatherController.java
+++ src/main/java/com/example/demo/weatherApi/WeatherController.java
@@ -1,4 +1,4 @@
-package com.example.demo.weatherAPI;
+package com.example.demo.weatherApi;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
---
+++ src/test/java/com/example/demo/weatherApiTest/WeatherApiTest.java
@@ -0,0 +1,18 @@
+package com.example.demo.weatherApiTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WeatherApiTest {
+
+	String serviceKey = "SUGpdq3ufUPY4EA%2Fy6EDsonRTsw4bY%2F9ZInDHY304YJ1hxQYOh1TfWoWy4kVz1L%2Fm9P7ImHvkKGyD83FxWpSjQ%3D%3D";
+	String dataType = "JSON";
+	String code = "11B20201";
+
+	@Test
+	public void getWeather() {
+		
+		
+	}
+}
