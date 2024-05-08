a4b349cbdf55cc51f302bab04f7795a13e2f14da
man-coding <chch2857@gmail.com>
2024-05-09 01:10:43 +0900

    test

--- src/main/java/com/example/demo/chat/config/ChatHandler.java
+++ src/main/java/com/example/demo/chat/config/ChatHandler.java
@@ -13,11 +13,11 @@ public class ChatHandler extends TextWebSocketHandler {
 	private static LinkedHashSet<WebSocketSession> numSet = new LinkedHashSet<>(); // 웹소켓 세션 저장할 해시셋 생성
 
 	@Override
-	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception{
-		
-		if(numSet.size()>=3) {
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+
+		if (numSet.size() >= 3) {
 			WebSocketSession oldSession = numSet.iterator().next(); // 가장 오래된 세션을 가져옴
-			
+
 		}
 	}
 
