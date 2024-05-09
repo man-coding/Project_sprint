791027cbb11d460d2d90fec1a2d397e762eabacd
ms <ms@DESKTOP-59NCD3P>
2024-05-10 00:48:56 +0900

    240510 채팅 구현중

--- src/main/java/com/example/demo/chat/controller/ChatController.java
+++ src/main/java/com/example/demo/chat/controller/ChatController.java
@@ -1,18 +1,24 @@
 package com.example.demo.chat.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import com.example.demo.chat.Message.ChatMessage;
+import com.example.demo.member.service.MemberService;
 
 @Controller
 public class ChatController {
-
-	@MessageMapping("/chat.private.{userId}")
-	@SendTo("/topic/public")
-	public ChatMessage sendMessage(ChatMessage chatMessage) {
+	
+	@Autowired
+	MemberService memberService;
+	
+	@MessageMapping("/chat/private-{userId}")
+	public ChatMessage sendMessage(ChatMessage chatMessage, Principal principal) {
 		
-		return chatMessage;
+		String senderId = principal.getName();
 	}
 }
